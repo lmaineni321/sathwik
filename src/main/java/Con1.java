d08c6f695c0e271a812e1e3ff97a252dc119ec1d
PAVANI <PAVANI@DESKTOP-T5RPGMH>
2022-05-06 11:35:07 +0530

    con1,2 added

---
+++ src/main/java/Con1.java
@@ -0,0 +1,9 @@
+
+public class Con1 {
+
+	public static void main(String[] args) {
+		System.out.println("con1 class");
+
+	}
+
+}
---
+++ src/main/java/Con2.java
@@ -0,0 +1,8 @@
+
+public class Con2 {
+
+	public static void main(String[] args) {
+		System.out.println("con2 class");
+	}
+
+}
